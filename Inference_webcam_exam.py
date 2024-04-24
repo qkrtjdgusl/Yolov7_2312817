17d3089dc7f6274a125042751fc9b4378d30889b
Seonghyun Park <140165337+qkrtjdgusl@users.noreply.github.com>
2024-04-24 18:24:51 +0900

    Update Inference_webcam_exam.py

--- Inference_webcam_exam.py
+++ Inference_webcam_exam.py
@@ -1,7 +1,7 @@
 # Inference for ONNX model
 
 import cv2
-import tensorflow.compat.v1 as tf
+import tensorflow as tf
 cuda = True
 w = "yolov7-tiny.onnx"
 #img = cv2.imread('horses.jpg')  # image-based execute!
@@ -18,11 +18,10 @@ from collections import OrderedDict,namedtuple
 providers = ['CUDAExecutionProvider', 'CPUExecutionProvider'] if cuda else ['CPUExecutionProvider'] #['AzureExecutionProvider', 'CPUExecutionProvider'] if cuda else ['CPUExecutionProvider']
 session = ort.InferenceSession(w, providers=providers)
 
-tf.compat.disable_v2_behavior()
-with tf.compat.Session() as sess:
-    x = tf.compat.placeholder(tf.float32, [2])
-    x2 = tf.square(x)
-    print(sess.run(x2, feed_dict={x: [2, 3]}))
+# TensorFlow 1.x 호환 가능하도록 수정정
+tf.compat.v1.disable_eager_execution()
+with tf.compat.v1.Session() as sess:
+    x = tf.compat.v1.placeholder(tf.float32, [2])
     # [4. 9.]
 
 #print("00000")
@@ -125,7 +124,7 @@ while webcam.isOpened():
         cv2.rectangle(image,box[:2],box[2:],color,2)
         cv2.putText(image,name,(box[0], box[1] - 2),cv2.FONT_HERSHEY_SIMPLEX,0.75,[225, 255, 255],thickness=2)  
 
-    print('[INFO] draw all detected boxes by Prof. Kim....!')    #<===== 여기에 여러분들의 학번이 표시되도록 합니다.
+    print('[INFO] draw all detected boxes by 2312817 SeongHyun Park....!')    #<===== 여기에 여러분들의 학번이 표시되도록 합니다.
 
     image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR) # PIL image --> opencv image Mat buffer
     
